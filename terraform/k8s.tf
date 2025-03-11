bf8e7bcf0b092a241320257c7f4d73c896c15514
Pavlo Lebediev <pavlo.lebediev@clouds365.io>
2025-03-11 22:00:46 +0100

    Enable geo-redundant backup for PostgreSQL and enhance Kubernetes configuration with automatic upgrades, key vault secrets, and improved node pool settings

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -22,19 +22,32 @@ resource "azurerm_kubernetes_cluster" "this" {
   local_account_disabled            = true
   role_based_access_control_enabled = true
   oidc_issuer_enabled               = true
+  automatic_upgrade_channel         = "stable"
   azure_active_directory_role_based_access_control {
     azure_rbac_enabled     = true
     admin_group_object_ids = ["32105cb5-16af-4fe5-9ab3-1b229906c701"]
   }
+  key_vault_secrets_provider {
+    secret_rotation_enabled = true
+  }
+  oms_agent {
+    enabled                    = true
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+  }
+  #azure_policy_enabled = true
 
   default_node_pool {
-    name                 = "system"
-    vm_size              = "Standard_D2s_v6"
-    vnet_subnet_id       = azurerm_subnet.this["k8s"].id
-    min_count            = 1
-    max_count            = 10
-    auto_scaling_enabled = true
-    orchestrator_version = "1.29"
+    name                         = "system"
+    vm_size                      = "Standard_D2s_v6"
+    vnet_subnet_id               = azurerm_subnet.this["k8s"].id
+    min_count                    = 1
+    max_count                    = 10
+    auto_scaling_enabled         = true
+    orchestrator_version         = "1.29"
+    os_disk_type                 = "Ephemeral"
+    max_pods                     = 50
+    enable_host_encryption       = true
+    only_critical_addons_enabled = true
     upgrade_settings {
       drain_timeout_in_minutes      = 0
       max_surge                     = "10%"
