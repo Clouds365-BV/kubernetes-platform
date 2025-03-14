93d1c85392e6204a718d4acda84fe35dc798e96f
Pavlo Lebediev <pavlo.lebediev@clouds365.io>
2025-03-14 13:32:39 +0100

    Update admin group object ID in Kubernetes configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -30,9 +30,27 @@ provider "azurerm" {
 
 provider "helm" {
   kubernetes {
-    host                   = azurerm_kubernetes_cluster.this.kube_admin_config.0.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config.0.client_key)
+    host                   = azurerm_kubernetes_cluster.this.kube_config.0.host
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config.0.cluster_ca_certificate)
+    #client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config.0.client_certificate)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "kubelogin"
+      args = [
+        "get-token",
+        "--login",
+        "spn",
+        "--environment",
+        "AzurePublicCloud",
+        "--tenant-id",
+        "$ARM_TENANT_ID",
+        "--server-id",
+        "6dae42f8–4368–4678–94ff-3960e28e3630",
+        "--client-id",
+        "$ARM_CLIENT_ID",
+        "--client-secret",
+        "$ARM_CLIENT_SECRET"
+      ]
+    }
   }
 }
