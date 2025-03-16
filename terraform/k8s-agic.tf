b4fc600e244e9cecd359d81c6cc715dc589e783e
Pavlo Lebediev <pavlo.lebediev@clouds365.io>
2025-03-16 11:03:14 +0100

    Add ingress application gateway configuration and remove critical addons setting

--- terraform/k8s-agic.tf
+++ terraform/k8s-agic.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "k8s-agic" {
   scope                = azurerm_subnet.this["app_gateway"].id
   role_definition_name = "Network Contributor"
-  principal_id         = azurerm_kubernetes_cluster.this.ingress_application_gateway[0].ingress_application_gateway_identity[0].principal_id
+  principal_id         = azurerm_kubernetes_cluster.this.ingress_application_gateway[0].ingress_application_gateway_identity[0]
 }
