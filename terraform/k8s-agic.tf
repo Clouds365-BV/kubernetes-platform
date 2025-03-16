f29900a8376e5f0f3963db5895ff1746f2c1c6b5
Pavlo Lebediev <pavlo.lebediev@clouds365.io>
2025-03-16 12:55:11 +0100

    Refactor role assignments for ingress application gateway to use local variables

--- terraform/k8s-agic.tf
+++ terraform/k8s-agic.tf
@@ -1,15 +1,15 @@
-locals {
-  agic_roles = {
-    "Network Contributor" : azurerm_subnet.this["app_gateway"].id,
-    "Reader" : azurerm_resource_group.this.id,
-    "Contributor" : azurerm_application_gateway.this.id
-  }
-}
-
-resource "azurerm_role_assignment" "k8s-agic" {
-  for_each = local.agic_roles
-
-  scope                = each.value
-  role_definition_name = each.key
-  principal_id         = azurerm_kubernetes_cluster.this.ingress_application_gateway[0].ingress_application_gateway_identity[0].client_id
-}
+# locals {
+#   agic_roles = {
+#     "Network Contributor" : azurerm_subnet.this["app_gateway"].id,
+#     "Reader" : azurerm_resource_group.this.id,
+#     "Contributor" : azurerm_application_gateway.this.id
+#   }
+# }
+#
+# resource "azurerm_role_assignment" "k8s-agic" {
+#   for_each = local.agic_roles
+#
+#   scope                = each.value
+#   role_definition_name = each.key
+#   principal_id         = azurerm_kubernetes_cluster.this.ingress_application_gateway[0].ingress_application_gateway_identity[0].client_id
+# }
