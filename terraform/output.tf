6fe9a37f13c6c96610b31bfc65c104bf84e6a1d8
Pavlo Lebediev <pavlo.lebediev@clouds365.io>
2025-03-16 11:12:13 +0100

    Add ingress application gateway configuration and remove critical addons setting

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
 output "environment_name" {
   value = var.env.name
 }
+
+output "application_gateway_identity" {
+  value = azurerm_kubernetes_cluster.this.ingress_application_gateway[0].ingress_application_gateway_identity[0]
+}
