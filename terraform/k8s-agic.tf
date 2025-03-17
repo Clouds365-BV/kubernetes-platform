3c3355fdc690115b0f7099b5283580fd21e9b231
Pavlo Lebediev <pavlo.lebediev@clouds365.io>
2025-03-17 16:23:45 +0100

    Add SSL certificate configuration for drones-shuttles and update App Gateway settings

--- terraform/k8s-agic.tf
+++ terraform/k8s-agic.tf
@@ -3,7 +3,8 @@ module "k8s-agic-roles" {
   for_each = {
     "Network Contributor" : azurerm_subnet.this["app_gateway"].id,
     "Reader" : azurerm_resource_group.this.id,
-    "Contributor" : azurerm_application_gateway.this.id
+    "Contributor" : azurerm_application_gateway.this.id,
+    "Contributor" : azurerm_user_assigned_identity.agw.id
   }
 
   object_id = azurerm_kubernetes_cluster.this.ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
