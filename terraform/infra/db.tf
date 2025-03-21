4f5ba46817f1de2fe8a6348554716565008fd3cd
Pavlo Lebediev <pavlo.lebediev@clouds365.io>
2025-03-21 13:54:20 +0100

    Replace PostgreSQL with MySQL flexible server configuration and update related secrets in Azure Key Vault

--- terraform/infra/db.tf
+++ terraform/infra/db.tf
@@ -1,14 +1,14 @@
 resource "azurerm_mysql_flexible_server" "this" {
-  name                          = "${local.resource_name_prefix}-psql"
-  location                      = azurerm_resource_group.this.location
-  resource_group_name           = azurerm_resource_group.this.name
-  administrator_login           = azurerm_key_vault_secret.this[var.env.databases.mysql.administrator_login].value
-  administrator_password        = azurerm_key_vault_secret.this[var.env.databases.mysql.administrator_password].value
-  delegated_subnet_id           = azurerm_subnet.this[var.env.databases.mysql.subnet].id
-  backup_retention_days         = 7
-  public_network_access_enabled = var.env.databases.mysql.public_network_access_enabled
-  sku_name                      = var.env.databases.mysql.sku_name
-  private_dns_zone_id           = azurerm_private_dns_zone.this["privatelink.mysql.database.azure.com"].id
+  name                   = "${local.resource_name_prefix}-psql"
+  location               = azurerm_resource_group.this.location
+  resource_group_name    = azurerm_resource_group.this.name
+  administrator_login    = azurerm_key_vault_secret.this[var.env.databases.mysql.administrator_login].value
+  administrator_password = azurerm_key_vault_secret.this[var.env.databases.mysql.administrator_password].value
+  delegated_subnet_id    = azurerm_subnet.this[var.env.databases.mysql.subnet].id
+  backup_retention_days  = 7
+  #public_network_access_enabled = var.env.databases.mysql.public_network_access_enabled
+  sku_name            = var.env.databases.mysql.sku_name
+  private_dns_zone_id = azurerm_private_dns_zone.this["privatelink.mysql.database.azure.com"].id
   storage {
     auto_grow_enabled = true
     size_gb           = var.env.databases.mysql.storage_gb
