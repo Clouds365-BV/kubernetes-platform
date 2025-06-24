897f0aea799da10dcd36384fc328437e7a72e177
Pavlo Lebediev <pavlo.lebediev@clouds365.io>
2025-06-24 19:07:46 +0200

    feat: add Drone CI/CD pipeline and restructure Ghost Cleaner deployment for improved organization

--- terraform-deployment/k8s/local.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  resource_name_prefix = "${var.project_name}-${var.env.name_short}-${var.env.location_short}"
-
-  tags = var.env.tags
-}
--- terraform-deployment/k8s/output.tf
+++
@@ -1,7 +0,0 @@
-output "environment_name" {
-  value = var.env.name
-}
-
-output "k8s_key_vault_secrets_provider_identity" {
-  value = data.azurerm_kubernetes_cluster.this.key_vault_secrets_provider[0].secret_identity[0]
-}
